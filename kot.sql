05d154a02c079cfd3eaf1a02164036c171c48f59
xvrafi <103484558+xvrafi@users.noreply.github.com>
2022-11-11 21:54:19 +0100

    Add files via upload

---
+++ kot.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE kot;
+use kot;
+
+
+
+create table kot(
+    Imie VARCHAR(90),
+    Wiek int(70),
+    Gatunek VARCHAR(10)
+   
+  
+    
+);
+
+
+insert into kot (Imie,Wiek,Gatunek) VALUES
+('Tosia','10','Pers'),
+('Mania','5','Turecki');
+
+
